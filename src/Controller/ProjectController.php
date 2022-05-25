13fbd98bfee750ae0a18da6d376802ff70429efa
Steven Essam <stevenessams7@gmail.com>
2022-05-25 22:41:38 +0200

    details

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -1,18 +1,50 @@
 <?php
 
 namespace App\Controller;
+use App\Entity\Project;
 use App\Repository\ProjectRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProjectController extends AbstractController
 {
     #[Route('/project', name: 'app_project')]
-    public function index(ProjectRepository $projectRepository): Response
-    {
+    public function index(
+        ProjectRepository $projectRepository,
+        PaginatorInterface $paginator,
+        Request $request
+
+        ): Response {
+
+        $data = $projectRepository->findAll();
+
+        $projects = $paginator->paginate(
+        $data,
+        $request->query->getInt('page',1),
+        9
+        );
+
         return $this->render('project/project.html.twig', [
-            'projects' => $projectRepository->findAll(),
+            'projects' => $projects ,
         ]);
     }
+
+    /**
+      *  @Route("/project/{slug}",name="app_project_details")
+      */
+    public function details(Project $project): Response
+    {
+
+        return $this->render('project/details.html.twig', [
+             'project'=>$project ,
+        ]);
+
+
+    }
+
+
+
 }
