187feeb242ea3dde879cc80674e0166e2209e5b1
Steven Essam <stevenessams7@gmail.com>
2022-06-04 17:02:37 +0200

    text

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -9,21 +9,40 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(Request $request, ContactService $contactService): Response
+    public function index(Request $request, MailerInterface $mailer): Response
     {
+
+        //ContactService $contactService,
 
         $contact = new Contact();
         $form = $this->createForm(ContactType::class,$contact);
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()){
-            $contact = $form->getData();
-            $contactService->persistContact($contact);
+            // $contact = $form->getData();
+            // $contactService->persistContact($contact);
+
+
+            $email = (new Email())
+            ->from('stevenessamlegion@hotmail.com')
+            ->to('manobalotalos@gmail.com')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            //->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+
+        $mailer->send($email);
+
+
 
             return $this->redirectToRoute('app_contact');
 
