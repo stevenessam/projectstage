0ae05e997c91a532c5f6f60de49fce223b335bc8
Steven Essam <stevenessams7@gmail.com>
2022-06-07 04:32:52 +0200

    s

--- src/EventSubscriber/EasyAdminSubscriber.php
+++ src/EventSubscriber/EasyAdminSubscriber.php
@@ -1,47 +1,47 @@
 <?php
 
-// namespace App\Entity\EventSubscriber;
+namespace App\Entity\EventSubscriber;
 
-// use DateTime;
-// use App\Entity\User;
-// use Symfony\Component\Security\Core\Security;
-// use Symfony\Component\String\Slugger\SluggerInterface;
-// use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-// use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityPersistedEvent;
+use DateTime;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityPersistedEvent;
 
-// class EasyAdminSubscriber implements EventSubscriberInterface
-// {
-//     private $slugger;
-//     private $security;
-//     public function __construct (SluggerInterface $slugger,Security $security)
-//     {
+class EasyAdminSubscriber implements EventSubscriberInterface
+{
+    private $slugger;
+    private $security;
+    public function __construct (SluggerInterface $slugger,Security $security)
+    {
     
-//         $this->slugger = $slugger;
-//         $this->security= $security;
-//     }
+        $this->slugger = $slugger;
+        $this->security= $security;
+    }
 
 
-//     public static function getSubscribedEvents()
-//     {
-//         return [
-//             BeforeEntityPersistedEvent::class => ['setDateAndUser'],
-//         ];
-//     }
+    public static function getSubscribedEvents()
+    {
+        return [
+            BeforeEntityPersistedEvent::class => ['setDateAndUser'],
+        ];
+    }
 
 
 
 
-//         public function setDateAndUser(BeforeEntityPersistedEvent $event)
-//         {
-//         $entity = $event->getEntityInstance();
+        public function setDateAndUser(BeforeEntityPersistedEvent $event)
+        {
+        $entity = $event->getEntityInstance();
 
-//             if(($entity instanceof Project)){
-//                     $now = new DateTime('now');
-//                     $entity->setCreatedAt($now);
-//                     $user = $this->security->getUser();
-//                     $entity->setUser($user);
-//             }
-//                 return;
+            if(($entity instanceof Project)){
+                    $now = new DateTime('now');
+                    $entity->setCreatedAt($now);
+                    $user = $this->security->getUser();
+                    $entity->setUser($user);
+            }
+                return;
 
-//                 }
-// }
+                }
+}
