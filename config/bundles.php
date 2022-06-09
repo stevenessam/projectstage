ca73a04b6ac265c2cfcb2ef4c9b74b5cf3458f55
Steven Essam <stevenessams7@gmail.com>
2022-06-09 10:19:25 +0200

    Revert "ssdddtest"
    This reverts commit 10390fb0dadb2534d3172dd0458215c1d58151d6.

--- config/bundles.php
+++ config/bundles.php
@@ -16,5 +16,4 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
-    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
 ];
